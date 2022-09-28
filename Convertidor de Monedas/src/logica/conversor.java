45e606d79a764eaeec887f8cfd75a87dc42dfee6
gunz2809 <54121864+gunz2809@users.noreply.github.com>
2022-09-28 20:53:25 -0300

    Add files via upload

---
+++ Convertidor de Monedas/src/logica/conversor.java
@@ -0,0 +1,57 @@
+package logica;
+
+import java.util.Scanner;
+
+public class conversor {
+
+/*Cotizacion
+        dolares = 42.5
+        peso argentino = 0,35
+        real = 9*/
+    
+ private int pesos;   
+
+ 
+
+ 
+ public double pesouyAdolar(int pesos){
+     
+     int dolar = 42;
+     int dolares = pesos/dolar;
+     return dolares;
+ }
+ 
+  
+ public double pesouyApesoarg(int pesos){
+     
+     double pesoArg = 0.35;
+     double pesosArg = pesos/pesoArg;
+     return pesosArg;
+ }
+ 
+  
+ public double pesouyAreal(int pesos){
+     
+     int real = 9;
+     int reales = pesos/real;
+     return reales;
+ }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+      
+        
+       
+    
+    
+    
+    
+}
